7c0709129ce4793983f2415389ebcb23836a366c
Marine DELPON <marine@macbook-pro-de-marine.home>
2022-12-11 18:18:21 +0100

    server.js implementation

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const app = require('./app');
+const connection = require('./mongoose');
+
+connection
+.then(() => console.log('connection à MongoDB réussie !'))
+.catch(() => {
+    console.log('connection à MongoDB échoué !');
+    process.exit(1);
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+});
