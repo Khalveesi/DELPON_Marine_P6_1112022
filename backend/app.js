792ed36980b2fcbe6bdf7492a7cde68f2d467ae4
Marine DELPON <marine@macbook-pro-de-marine.home>
2022-12-11 19:21:26 +0100

    auth route done

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const userRoutes = require('./routes/user.route');
+
+app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
     next();
-  });
+});
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
-const app = require('./app');
-const connection = require('./mongoose');
+require("dotenv").config();
+const app = require("./app");
+const connection = require("./mongoose");
 
 connection
-.then(() => console.log('connection à MongoDB réussie !'))
-.catch(() => {
-    console.log('connection à MongoDB échoué !');
-    process.exit(1);
-})
+    .then(() => console.log("connection à MongoDB réussie !"))
+    .catch(() => {
+        console.log("connection à MongoDB échoué !");
+        process.exit(1);
+    });
 
 const port = process.env.PORT || 3000;
 
