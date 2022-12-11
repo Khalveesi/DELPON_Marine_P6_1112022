51968ecff2d9281f94a73fdcfb98fca45ed9d674
Marine DELPON <marine@macbook-pro-de-marine.home>
2022-12-11 17:23:28 +0100

    boostrap / sauce and user models

---
+++ backend/models/User.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const UniqueValidator = require('mongosse-unique-validator');
+
+const userSchema = mongoose.Schema({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true }
+});
+
+userSchema.plugin(UniqueValidator);
+module.exports = mongoose.model('User', userSchema); 
